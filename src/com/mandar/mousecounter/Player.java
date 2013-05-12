d89ffdd6729b5e211e0a2de7c77693c23b2a299f
Mandar Mulherkar <mandar.mulherkar@gmail.com>
2013-05-12 12:58:57 -0700

    some timing thing

--- src/com/mandar/mousecounter/Player.java
+++ src/com/mandar/mousecounter/Player.java
@@ -3,11 +3,18 @@ package com.mandar.mousecounter;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
 
 import uk.co.caprica.vlcj.binding.LibVlc;
@@ -24,7 +31,8 @@ public class Player {
 
 	private static EmbeddedMediaPlayer mediaPlayer = null;
 	private MediaPlayerFactory mediaPlayerFactory = null;
-	
+	private static Label statusLabel = new Label("Status");
+    
 	
     public static void main(final String[] args) {
         NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), "MacOS/lib");
@@ -37,20 +45,66 @@ public class Player {
 
                 boolean test = mediaPlayer.playMedia("/Users/mulherka/Downloads/bailey.mpg");
                 System.out.println(""+test);
+                setLabel();
             }
         });
         
     }
 
-    private Player(String[] args) {
+    protected static void setLabel() {
+    	
+    	Thread thread = new Thread(new Runnable(){
+
+    		
+    		
+    		
+			@Override
+			public void run() {
+				
+			
+				try {
+					Thread.sleep(0);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				while(mediaPlayer.isPlaying())
+		        {
+		        	long millis=mediaPlayer.getTime();
+		        	String s = String.format("%02d:%02d:%02d", //dont know why normal Java date utils doesn't format the time right
+				      TimeUnit.MILLISECONDS.toHours(millis),
+				      TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), 
+				      TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
+				    );
+		            //setTitle(ms.format(new Time(sec)));
+		        	statusLabel.setText(s);
+//		            syncTimeline=true;
+//		            timeline.setValue(Math.round(mp.getPosition()*100));
+//		            syncTimeline=false;
+//		            notifyObservers(mp.getTime());
+		        }
+				
+				
+				
+				
+			}
+    	});
+    	
+    	thread.start();
+		
+	}
+
+	private Player(String[] args) {
 
     	JFrame frame = new JFrame("MouseCounter");
+    	frame.getContentPane().setFont(new Font("Helvetica", Font.PLAIN, 13));
+    	frame.setFont(new Font("Helvetica", Font.PLAIN, 12));
     	JPanel playerPanel = new JPanel(new BorderLayout());
     	playerPanel.setBorder(BorderFactory.createTitledBorder("VLC Player"));
     	
     	mediaPlayerFactory = new MediaPlayerFactory();
     	
         Canvas canvas = new Canvas();
+        canvas.setFont(new Font("Helvetica", Font.PLAIN, 12));
         canvas.setSize(640, 480);
         canvas.setBackground(Color.black);
         playerPanel.add(canvas);
@@ -63,15 +117,31 @@ public class Player {
 
         mediaPlayer.setVideoSurface(mediaPlayerFactory.newVideoSurface(canvas));
         
-        JPanel panel = new JPanel(new BorderLayout());
+        JPanel panel = new JPanel();
         frame.getContentPane().add(panel, BorderLayout.EAST);
-        JButton button = new JButton("Click me!");
+        JButton button = new JButton("click me!");
+        button.setFont(new Font("Helvetica", Font.PLAIN, 13));
+        button.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		if(mediaPlayer.canPause()){
+        			mediaPlayer.pause();
+        		}else{
+        			mediaPlayer.play();
+        		}
+        	}
+        });
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        
+        statusLabel.setFont(new Font("Helvetica", Font.PLAIN, 12));
+        panel.add(statusLabel);
         panel.add(button);
         
-        JPanel panel1 = new JPanel(new BorderLayout());
+        JPanel panel1 = new JPanel();
         frame.getContentPane().add(panel1, BorderLayout.SOUTH);
-        JButton button1 = new JButton("Click me!");
-        panel1.add(button1);
+        panel1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        
+        JSlider slider = new JSlider();
+        panel1.add(slider);
         
         
         mediaPlayer.setPlaySubItems(true);
