f58e12efa32d20424478ac56789c8286db856822
Mandar Mulherkar <mandar.mulherkar@gmail.com>
2013-05-14 02:44:51 -0700

    adding icons

--- src/com/mandar/mousecounter/Player.java
+++ src/com/mandar/mousecounter/Player.java
@@ -29,12 +29,21 @@ import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.ImageIcon;
 
 public class Player {
 
 	private static EmbeddedMediaPlayer mediaPlayer = null;
 	private MediaPlayerFactory mediaPlayerFactory = null;
-	private static Label statusLabel = new Label("Status");
     
 	
     public static void main(final String[] args) {
@@ -48,65 +57,22 @@ public class Player {
 
                 boolean test = mediaPlayer.playMedia("/Users/mulherka/Downloads/bailey.mpg");
                 System.out.println(""+test);
-                setLabel();
             }
         });
         
     }
 
-    protected static void setLabel() {
-    	
-    	Thread thread = new Thread(new Runnable(){
-
-    		
-    		
-    		
-			@Override
-			public void run() {
-				
-			
-				try {
-					Thread.sleep(0);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				while(mediaPlayer.isPlaying())
-		        {
-		        	long millis=mediaPlayer.getTime();
-		        	String s = String.format("%02d:%02d:%02d", //dont know why normal Java date utils doesn't format the time right
-				      TimeUnit.MILLISECONDS.toHours(millis),
-				      TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), 
-				      TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
-				    );
-		            //setTitle(ms.format(new Time(sec)));
-		        	statusLabel.setText(s);
-//		            syncTimeline=true;
-//		            timeline.setValue(Math.round(mp.getPosition()*100));
-//		            syncTimeline=false;
-//		            notifyObservers(mp.getTime());
-		        }
-				
-				
-				
-				
-			}
-    	});
-    	
-    	thread.start();
-		
-	}
-
 	private Player(String[] args) {
 
-    	JFrame frame = new JFrame("MouseCounter");
+		//mediaPlayerFactory = new MediaPlayerFactory();
+		
+		JFrame frame = new JFrame("MouseCounter");
     	frame.getContentPane().setFont(new Font("Helvetica", Font.PLAIN, 13));
     	frame.setFont(new Font("Helvetica", Font.PLAIN, 12));
     	JPanel playerPanel = new JPanel(new BorderLayout());
     	playerPanel.setBorder(BorderFactory.createTitledBorder("VLC Player"));
     	
-    	mediaPlayerFactory = new MediaPlayerFactory();
-    	
-        Canvas canvas = new Canvas();
+    	Canvas canvas = new Canvas();
         canvas.setFont(new Font("Helvetica", Font.PLAIN, 12));
         canvas.setSize(640, 480);
         canvas.setBackground(Color.black);
@@ -121,36 +87,224 @@ public class Player {
         mediaPlayer.setVideoSurface(mediaPlayerFactory.newVideoSurface(canvas));
         
         JPanel panel = new JPanel();
-        frame.getContentPane().add(panel, BorderLayout.EAST);
-        JButton button = new JButton("click me!");
-        button.setFont(new Font("Helvetica", Font.PLAIN, 13));
-        button.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-        		if(mediaPlayer.canPause()){
-        			mediaPlayer.pause();
-        		}else{
-        			mediaPlayer.play();
-        		}
-        	}
-        });
-        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        playerPanel.add(panel, BorderLayout.EAST);
+        GridBagLayout gbl_panel = new GridBagLayout();
+        gbl_panel.columnWidths = new int[]{0, 0, 0, 0};
+        gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+        panel.setLayout(gbl_panel);
         
-        statusLabel.setFont(new Font("Helvetica", Font.PLAIN, 12));
-        panel.add(statusLabel);
-        panel.add(button);
+        JPanel activityButtonsPanel = new JPanel();
+        activityButtonsPanel.setBorder(BorderFactory.createTitledBorder("Behavior"));
+        GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+        gbc_panel_2.gridheight = 7;
+        gbc_panel_2.gridwidth = 3;
+        gbc_panel_2.insets = new Insets(0, 0, 5, 0);
+        gbc_panel_2.fill = GridBagConstraints.BOTH;
+        gbc_panel_2.gridx = 0;
+        gbc_panel_2.gridy = 1;
+        panel.add(activityButtonsPanel, gbc_panel_2);
+        GridBagLayout gbl_activityButtonsPanel = new GridBagLayout();
+        gbl_activityButtonsPanel.columnWidths = new int[]{0, 0, 0, 0};
+        gbl_activityButtonsPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+        gbl_activityButtonsPanel.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_activityButtonsPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        activityButtonsPanel.setLayout(gbl_activityButtonsPanel);
         
-        JPanel panel1 = new JPanel();
-        frame.getContentPane().add(panel1, BorderLayout.SOUTH);
-        panel1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        JButton btnNewButton_1 = new JButton("Lick");
+        btnNewButton_1.setIcon(new ImageIcon(Player.class.getResource("/icons/icecream.png")));
+        GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+        gbc_btnNewButton_1.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
+        gbc_btnNewButton_1.gridx = 1;
+        gbc_btnNewButton_1.gridy = 0;
+        activityButtonsPanel.add(btnNewButton_1, gbc_btnNewButton_1);
+        
+        JButton btnNewButton = new JButton("Flinch");
+        btnNewButton.setIcon(new ImageIcon(Player.class.getResource("/icons/flinch.png")));
+        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+        gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+        gbc_btnNewButton.gridx = 2;
+        gbc_btnNewButton.gridy = 0;
+        activityButtonsPanel.add(btnNewButton, gbc_btnNewButton);
+        
+        JButton btnNewButton_2 = new JButton("Scratch");
+        btnNewButton_2.setIcon(new ImageIcon(Player.class.getResource("/icons/scratch.png")));
+        GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+        gbc_btnNewButton_2.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
+        gbc_btnNewButton_2.gridx = 1;
+        gbc_btnNewButton_2.gridy = 1;
+        activityButtonsPanel.add(btnNewButton_2, gbc_btnNewButton_2);
+        
+        JButton btnNewButton_3 = new JButton("Sniff");
+        btnNewButton_3.setIcon(new ImageIcon(Player.class.getResource("/icons/rose.png")));
+        GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
+        gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 0);
+        gbc_btnNewButton_3.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnNewButton_3.gridx = 2;
+        gbc_btnNewButton_3.gridy = 1;
+        activityButtonsPanel.add(btnNewButton_3, gbc_btnNewButton_3);
+        
+        JButton btnGuard = new JButton("Guard");
+        btnGuard.setIcon(new ImageIcon(Player.class.getResource("/icons/guard.png")));
+        GridBagConstraints gbc_btnGuard = new GridBagConstraints();
+        gbc_btnGuard.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnGuard.insets = new Insets(0, 0, 5, 5);
+        gbc_btnGuard.gridx = 1;
+        gbc_btnGuard.gridy = 2;
+        activityButtonsPanel.add(btnGuard, gbc_btnGuard);
+        
+        JButton btnNewButton_4 = new JButton("Lift");
+        btnNewButton_4.setIcon(new ImageIcon(Player.class.getResource("/icons/lift.png")));
+        GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
+        gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 0);
+        gbc_btnNewButton_4.gridx = 2;
+        gbc_btnNewButton_4.gridy = 2;
+        activityButtonsPanel.add(btnNewButton_4, gbc_btnNewButton_4);
+        
+        JButton btnSomething = new JButton("Wipe");
+        btnSomething.setIcon(new ImageIcon(Player.class.getResource("/icons/wipe.png")));
+        GridBagConstraints gbc_btnSomething = new GridBagConstraints();
+        gbc_btnSomething.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnSomething.insets = new Insets(0, 0, 5, 5);
+        gbc_btnSomething.gridx = 1;
+        gbc_btnSomething.gridy = 3;
+        activityButtonsPanel.add(btnSomething, gbc_btnSomething);
+        
+        JSpinner spinner = new JSpinner();
+        GridBagConstraints gbc_spinner = new GridBagConstraints();
+        gbc_spinner.fill = GridBagConstraints.HORIZONTAL;
+        gbc_spinner.insets = new Insets(0, 0, 5, 0);
+        gbc_spinner.gridx = 2;
+        gbc_spinner.gridy = 3;
+        activityButtonsPanel.add(spinner, gbc_spinner);
+        
+        JButton btnNewBehavior = new JButton("New Behavior");
+        GridBagConstraints gbc_btnNewBehavior = new GridBagConstraints();
+        gbc_btnNewBehavior.insets = new Insets(0, 0, 5, 0);
+        gbc_btnNewBehavior.gridwidth = 2;
+        gbc_btnNewBehavior.fill = GridBagConstraints.HORIZONTAL;
+        gbc_btnNewBehavior.gridx = 1;
+        gbc_btnNewBehavior.gridy = 4;
+        activityButtonsPanel.add(btnNewBehavior, gbc_btnNewBehavior);
+        
+        JButton btnPlaypauserecord = new JButton("play/pause/record");
+        GridBagConstraints gbc_btnPlaypauserecord = new GridBagConstraints();
+        gbc_btnPlaypauserecord.gridwidth = 2;
+        gbc_btnPlaypauserecord.gridx = 1;
+        gbc_btnPlaypauserecord.gridy = 5;
+        activityButtonsPanel.add(btnPlaypauserecord, gbc_btnPlaypauserecord);
+        
+        JLabel label = new JLabel("");
+        GridBagConstraints gbc_label = new GridBagConstraints();
+        gbc_label.insets = new Insets(0, 0, 5, 5);
+        gbc_label.gridx = 0;
+        gbc_label.gridy = 2;
+        panel.add(label, gbc_label);
+        
+        JPanel currentValuesPanel = new JPanel();
+        currentValuesPanel.setBorder(BorderFactory.createTitledBorder("Current Values"));
+        GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+        gbc_panel_1.gridheight = 5;
+        gbc_panel_1.gridwidth = 3;
+        gbc_panel_1.fill = GridBagConstraints.BOTH;
+        gbc_panel_1.gridx = 0;
+        gbc_panel_1.gridy = 8;
+        panel.add(currentValuesPanel, gbc_panel_1);
+        GridBagLayout gbl_currentValuesPanel = new GridBagLayout();
+        gbl_currentValuesPanel.columnWidths = new int[]{0, 0, 0, 0, 0};
+        gbl_currentValuesPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+        gbl_currentValuesPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_currentValuesPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        currentValuesPanel.setLayout(gbl_currentValuesPanel);
+        
+        JLabel lblNewLabel_1 = new JLabel("Start Time");
+        GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+        gbc_lblNewLabel_1.fill = GridBagConstraints.HORIZONTAL;
+        gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_1.gridx = 0;
+        gbc_lblNewLabel_1.gridy = 0;
+        currentValuesPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+        
+        JLabel startTimeValue = new JLabel("0");
+        GridBagConstraints gbc_startTimeValue = new GridBagConstraints();
+        gbc_startTimeValue.insets = new Insets(0, 0, 5, 5);
+        gbc_startTimeValue.gridx = 2;
+        gbc_startTimeValue.gridy = 0;
+        currentValuesPanel.add(startTimeValue, gbc_startTimeValue);
+        
+        JLabel lblNewLabel_2 = new JLabel("Stop Time");
+        GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+        gbc_lblNewLabel_2.fill = GridBagConstraints.HORIZONTAL;
+        gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_2.gridx = 0;
+        gbc_lblNewLabel_2.gridy = 1;
+        currentValuesPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+        
+        JLabel stopTimeValue = new JLabel("0");
+        GridBagConstraints gbc_stopTimeValue = new GridBagConstraints();
+        gbc_stopTimeValue.insets = new Insets(0, 0, 5, 5);
+        gbc_stopTimeValue.gridx = 2;
+        gbc_stopTimeValue.gridy = 1;
+        currentValuesPanel.add(stopTimeValue, gbc_stopTimeValue);
+        
+        JLabel lblTotalTime = new JLabel("Total Time");
+        GridBagConstraints gbc_lblTotalTime = new GridBagConstraints();
+        gbc_lblTotalTime.fill = GridBagConstraints.HORIZONTAL;
+        gbc_lblTotalTime.insets = new Insets(0, 0, 5, 5);
+        gbc_lblTotalTime.gridx = 0;
+        gbc_lblTotalTime.gridy = 2;
+        currentValuesPanel.add(lblTotalTime, gbc_lblTotalTime);
+        
+        JLabel totalBehaviorTime = new JLabel("0");
+        GridBagConstraints gbc_totalBehaviorTime = new GridBagConstraints();
+        gbc_totalBehaviorTime.insets = new Insets(0, 0, 5, 5);
+        gbc_totalBehaviorTime.gridx = 2;
+        gbc_totalBehaviorTime.gridy = 2;
+        currentValuesPanel.add(totalBehaviorTime, gbc_totalBehaviorTime);
+        
+        JLabel lblBehavior = new JLabel("Behavior");
+        GridBagConstraints gbc_lblBehavior = new GridBagConstraints();
+        gbc_lblBehavior.fill = GridBagConstraints.HORIZONTAL;
+        gbc_lblBehavior.insets = new Insets(0, 0, 5, 5);
+        gbc_lblBehavior.gridx = 0;
+        gbc_lblBehavior.gridy = 3;
+        currentValuesPanel.add(lblBehavior, gbc_lblBehavior);
+        
+        JLabel label_4 = new JLabel("0");
+        GridBagConstraints gbc_label_4 = new GridBagConstraints();
+        gbc_label_4.insets = new Insets(0, 0, 5, 5);
+        gbc_label_4.gridx = 2;
+        gbc_label_4.gridy = 3;
+        currentValuesPanel.add(label_4, gbc_label_4);
+        
+        JLabel lblValue = new JLabel("Value");
+        GridBagConstraints gbc_lblValue = new GridBagConstraints();
+        gbc_lblValue.fill = GridBagConstraints.HORIZONTAL;
+        gbc_lblValue.insets = new Insets(0, 0, 0, 5);
+        gbc_lblValue.gridx = 0;
+        gbc_lblValue.gridy = 4;
+        currentValuesPanel.add(lblValue, gbc_lblValue);
+        
+        JPanel videoControlPanel = new JPanel();
+        videoControlPanel.setBorder(BorderFactory.createTitledBorder("Video Controls"));
+        frame.getContentPane().add(videoControlPanel, BorderLayout.SOUTH);
+        videoControlPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        
+        JLabel lblTime = new JLabel("Time");
+        videoControlPanel.add(lblTime);
         
         JSlider slider = new JSlider();
-        panel1.add(slider);
+        videoControlPanel.add(slider);
         
         
         mediaPlayer.setPlaySubItems(true);
         
         frame.setLocation(100, 100);
-        frame.setSize(640, 480);
+        frame.setSize(720, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
